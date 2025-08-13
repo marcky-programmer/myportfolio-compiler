28bc1d56e65f8adb2f2aae83126623417fad9b0f
marcky-programmer <leanmarc543@gmail.com>
2025-08-13 13:03:00 +0800

    Update Code_Challenge1.py

--- Code_Challenge1.py
+++ Code_Challenge1.py
@@ -1,3 +1,2 @@
-name = input("Type your name:" )
-
-print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\tHi\t\t\t\t*\n\t*\t\t\t\t",name,"\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
+name = input("Type your name here --->")
+print(f"\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t*\t\t\t Hi \f{name} \t\t\t\t*\n\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t*")
