637ecb98c84e751c6a8c8efb190d7de8dde56112
Lean Marc Alcala <leanmarc543@gamail.com>
2025-08-15 19:20:42 +0800

    updated

---
+++ code_challenge2.py
@@ -0,0 +1,36 @@
+amount = eval(input( "Enter amount to deposit -->" ))
+
+print("Herei is a breakdown of the deposit amount:  ")
+
+a = 1000
+b = 500
+c = 200  
+d = 100
+e = 50
+f = 20
+g = 10
+h = 5
+i = 1
+
+                                    #example 3816
+print( amount // a , "-" ,a)        #3.816 -> 3
+amount = amount % a                 #3.816 -> 816
+print( amount // b , "-" ,b) 
+amount = amount % b
+print( amount // c , "-" ,c) 
+amount = amount % c
+print( amount // d , "-" ,d) 
+amount = amount % d
+print( amount // e , "-" ,e) 
+amount = amount % e
+print( amount // f , "-" ,f) 
+amount = amount % f
+print( amount // g , "-" ,g) 
+amount = amount % g
+print( amount // h , "-" ,h) 
+amount = amount % h
+print( amount // i , "-" ,i) 
+amount = amount % i
+
+#// get the base form example 1.6180339887 = 1
+#% get the decimal or the remainder 1.6180339887 = .6180339887
