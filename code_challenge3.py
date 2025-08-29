9a9399db83836fce7748224fd2331fcb6d1f0a97
Lean Marc Alcala <leanmarc543@gamail.com>
2025-08-29 19:18:17 +0800

    updated

--- code_challenge3.py
+++ code_challenge3.py
@@ -3,10 +3,12 @@ print("from Lucena to Sariaya is 20 pero merong student discount".upper())
 isStudent = input("Are you a student? (yes or no) ---> ")
 
 if isStudent.lower() == "yes":
-    print("Your eligible for student discount", name)
+    print("Congratsn your eligible for student discount", name)
     fare = 20 * .2
     new_fare = 20 - fare
     print("Dahil ikaw ay student ang babayaran mo nalang ay", new_fare)
     bayad = eval(input("Enter your bayad ---> "))
+    sukli = bayad - new_fare 
+    print("Your change is", sukli)
 else:
     print("Sorry, your not eligible for student discount", name, ":<")
