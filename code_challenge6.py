f81bc8ca54db2a4b95eddc8126f13a21e500ad20
Lean Marc Alcala <leanmarc543@gamail.com>
2025-09-15 22:14:24 +0800

    updated

--- code_challenge6.py
+++ code_challenge6.py
@@ -1,6 +1,6 @@
 print("ODD number summation")
 sum = 0
-for i in range(1,8,1):
+for i in range(0,10,1):
     number = eval(input("Enter a number --> "))
     if number % 2 != 0:
          sum += number
