be4a337eb220538c721c1949b0c49bf242908e85
Lean Marc Alcala <leanmarc543@gamail.com>
2025-09-24 13:47:27 +0800

    commited

---
+++ activity15.py
@@ -0,0 +1,5 @@
+fname = input("Enter your first name here --> ")
+mname = input("Enter your middle name here --> ")
+lname = input("Enter your last name here --> ")
+
+print(f" Your name is {fname.upper()} {mname.upper()} {lname.upper()}")
---
+++ activity17.py
@@ -0,0 +1,11 @@
+for i in range(1,11,1):
+    for x in range(1,11,1):
+        print(x, end=" ")
+    print()
+
+
+for i in range(1,11,1):
+    print(i, end="-")
+    for x in range(1,11,1):
+        print(x, end=" ")
+    print()
