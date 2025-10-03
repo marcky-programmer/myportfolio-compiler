e396e9594fa526a93a39b0f4128cd11e8a1749a8
Lean Marc Alcala <leanmarc543@gamail.com>
2025-10-03 20:36:49 +0800

    committed

---
+++ code_challenge11.py
@@ -0,0 +1,9 @@
+
+for i in range(1,11,1):
+    for x in range(10,i,-1):
+        print(" ",end=" ")
+    for y in range(i,0,-1):
+        print("$",end=" ")
+    for z in range(1,i,1):
+        print("$", end=" ")
+    print()
