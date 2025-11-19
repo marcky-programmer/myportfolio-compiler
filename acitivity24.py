cb80458e04ccf6fcc325633179a07231b4b1e061
marcky-programmer <leanmarc543@gmail.com>
2025-11-19 16:00:49 +0800

    Add files via upload

---
+++ acitivity24.py
@@ -0,0 +1,9 @@
+from activity24_1  import GreetPersonName , GreetInfo
+
+def GreetPerson():
+    print("Hello !! kamusta ang buhay buhay")
+    print("just check it out")
+
+GreetPerson()
+# GreetPersonName("Lean")
+# GreetInfo('Marc', 'sariaya', '18')
