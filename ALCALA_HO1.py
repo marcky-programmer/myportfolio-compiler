4422034d9f0da7dc0f26e942f8b66f83831b944f
Lean Marc Alcala <leanmarc543@gamail.com>
2026-01-30 21:48:27 +0800

    committed

---
+++ ALCALA_HO1.py
@@ -0,0 +1,22 @@
+def word_Length_vs_Number_Average():
+    name = input('Whatts your name? boyyyy!!! -> ')
+    haba = len(name)
+    box = []
+    average = 0
+
+    for i in range(1,haba+1,1):
+        number = eval(input(f'Enter a number {i} : '))
+        average += number
+        box.append(number)
+    print(box)
+    print(f'The length of the word is {haba}')
+    print(f'The average of the number is {average}')
+    if haba == average:
+        print(f'The length of the word "{name}" is equal to the average.')
+    elif haba > average:
+        print(f'The length of the word "{name}" is greater than the average.')
+    else:
+        print(f'The length of the word "{name}" is less than the average.')
+        print('lala')
+
+word_Length_vs_Number_Average()
